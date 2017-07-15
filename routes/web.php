08027e1c622b6004de5848a85af17394d3c26543
return1225 <return1225@gmail.com>
2017-07-15 11:29:03 +0800

    setting up admin-page including css, js and layout, and modify app.scss for customization

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('/admin/users', 'AdminUsersController');
+
+Route::get('/admin', function(){
+
+	return view('admin.index');
+
+});
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,25 @@ const { mix } = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+
+   .scripts([
+
+	'resources/assets/js/libs/jquery.js',
+	'resources/assets/js/libs/bootstrap.js',
+	'resources/assets/js/libs/metisMenu.js',
+	'resources/assets/js/libs/sb-admin-2.js',
+	'resources/assets/js/libs/scripts.js'
+
+	], 'public/js/libs.js')
+
+   .styles([
+
+   	'resources/assets/css/libs/blog-post.css',
+	'resources/assets/css/libs/bootstrap.css',
+	'resources/assets/css/libs/font-awesome.css',
+	'resources/assets/css/libs/metisMenu.css',
+	'resources/assets/css/libs/sb-admin-2.css',
+	'resources/assets/css/libs/styles.css',
+
+   	], 'public/css/libs.css');
